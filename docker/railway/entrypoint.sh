64534a2b79fc384a3820fe753269b6ec35456255
JoaoJales <joaoricardocirino@gmail.com>
2026-05-06 20:19:59 -0300

    fix entrypoint.sh

--- docker/railway/entrypoint.sh
+++ docker/railway/entrypoint.sh
@@ -8,4 +8,11 @@ mkdir -p /etc/nginx/http.d
 
 envsubst '${PORT}' < /etc/nginx/templates/default.conf.template > /etc/nginx/http.d/default.conf
 
+php artisan migrate --force
+
+if [ "$RUN_SEED" = "true" ]; then
+  php artisan db:seed --force
+  php artisan db:seed --class=TestDataSeeder --force
+fi
+
 exec /usr/bin/supervisord -c /etc/supervisord.conf
